13c81899b85d7bfd05380468c659cc81ffde6fe5
Ethan D'Souza <91471122+ethandsz@users.noreply.github.com>
2025-05-22 15:46:37 +0100

    interactive and can pass in variables now

--- convec-diffusion-3d-2.0.py
+++ convec-diffusion-3d-2.0.py
@@ -17,20 +17,24 @@ def str2bool(v):
 
 
 NUM_CELLS = 50
-POLLUTANT_THRESHOLD = 0.05
+POLLUTANT_DETECTION_THRESHOLD = 0.05
 
 parser = argparse.ArgumentParser(prog='PDE Simulation', usage='%(PDE Simulation) [options]')
 parser.add_argument('--useMatplot', type=str2bool, nargs='?', const=True, default=False, help='Enable matplot viewer for a lighter weight simulation')
 parser.add_argument('--useMayavi', type=str2bool, nargs='?', const=True, default=True, help='Disable/Enable viewer')
+parser.add_argument('--simSteps', type=int, default=300, help='Number of simulation steps')
+parser.add_argument('--xFlow', type=float, default=0.0, help='Flow in X direction')
+parser.add_argument('--yFlow', type=float, default=0.0, help='Flow in Y direction')
+parser.add_argument('--zFlow', type=float, default=1.0, help='Flow in Z direction')
 
 args = parser.parse_args()
 useMayavi = args.useMayavi
 useMatplot = args.useMatplot
 
 diffusionCoef = 0.1
-convectionCoef = (1.0, 1.0, 1.0)
+convectionCoef = (args.xFlow, args.yFlow, args.zFlow)
 dt = 0.01
-steps = 300
+steps = args.simSteps
 
 nx = ny = nz = NUM_CELLS # number of cells 
 L = 1.0
@@ -64,17 +68,23 @@ viewer = None
 if useMayavi:
     viewer = Viewer(vars=var)
     # viewer = Viewer(vars=source)
+elif useMatplot:
+    plt.ion()
+
+fig = None
+ax = None
 
 input("Start sim?")
 
 pollutant_position_overtime_x = []
 pollutant_position_overtime_y = []
 pollutant_position_overtime_z = []
+
 for step in range(steps):
     var.updateOld()
     eq.solve(var=var, dt=dt)
     print(f"Step {step}")
-    if useMatplot and step % 5 == 0:
+    if useMatplot:
         num_cells = nx * ny * nz
         slice_z = num_cells/nz
         body = np.array(var)
@@ -87,6 +97,7 @@ for step in range(steps):
         x_unique = np.unique(x_vals)
         avg_values_x_slice = []
 
+        #Extracting average pollutant values across all x slices
         for x_val in x_unique:
             mask = np.isclose(x_vals, x_val)
             avg_x = values[mask].mean()
@@ -108,30 +119,26 @@ for step in range(steps):
             avg_z = values[mask].mean()
             avg_values_z_slice.append(avg_z)
 
-        ax = plt.figure().add_subplot(projection='3d')
-
-
+        #List to store the 'grid' of the body of water which is used later to show pollutant positions
         positions = np.linspace(0,1,NUM_CELLS)
 
         #Get the index of the pollutant if greater than a threshold
-        print("Average X Slices: ", avg_values_x_slice)
         for p in avg_values_x_slice:
-            if p > POLLUTANT_THRESHOLD:
+            if p > POLLUTANT_DETECTION_THRESHOLD:
+                #Index of the pollutant accross the x slice
                 index_of_pollutant = avg_values_x_slice.index(p)
-                print("Index of pollutant x: ", index_of_pollutant)
                 pollutant_position_overtime_x.append(positions[index_of_pollutant])
 
+                #Get the indices of the max values of the other slices since consider these to be above the x axis threshold
                 current_y_position_index = avg_values_y_slice.index(max(avg_values_y_slice))
                 current_z_position_index = avg_values_z_slice.index(max(avg_values_z_slice))
                 pollutant_position_overtime_y.append(positions[current_y_position_index])
                 pollutant_position_overtime_z.append(positions[current_z_position_index])
 
 
-        print("Average Y Slices: ", avg_values_y_slice)
         for p in avg_values_y_slice:
-            if p > POLLUTANT_THRESHOLD:
+            if p > POLLUTANT_DETECTION_THRESHOLD:
                 index_of_pollutant = avg_values_y_slice.index(p)
-                print("Index of pollutant y: ", index_of_pollutant)
                 pollutant_position_overtime_y.append(positions[index_of_pollutant])
 
                 current_x_position_index = avg_values_x_slice.index(max(avg_values_x_slice))
@@ -140,11 +147,9 @@ for step in range(steps):
                 pollutant_position_overtime_z.append(positions[current_z_position_index])
 
 
-        print("Average Z Slices: ", avg_values_z_slice)
         for p in avg_values_z_slice:
-            if p > POLLUTANT_THRESHOLD:
+            if p > POLLUTANT_DETECTION_THRESHOLD:
                 index_of_pollutant = avg_values_z_slice.index(p)
-                print("Index of pollutant z: ", index_of_pollutant)
                 pollutant_position_overtime_z.append(positions[index_of_pollutant])
 
 
@@ -153,21 +158,26 @@ for step in range(steps):
                 pollutant_position_overtime_x.append(positions[current_x_position_index])
                 pollutant_position_overtime_y.append(positions[current_y_position_index])
 
+        if fig is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(projection='3d')
+        else:
+            ax.clear()
+
 
         ax.scatter(pollutant_position_overtime_x, pollutant_position_overtime_y, pollutant_position_overtime_z, label='pollutant')
-
-
-        colors = ('r', 'g', 'b', 'k')
+        ax.set_title("Contaminated zone")
 
         ax.legend()
-        ax.set_xlim(0, 1)
-        ax.set_ylim(0, 1)
-        ax.set_zlim(0, 1)
+        ax.set_xlim(0, L)
+        ax.set_ylim(0, L)
+        ax.set_zlim(0, L)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
+        plt.draw()
+        plt.pause(0.001)  # Small pause to allow GUI to update
 
-        plt.show()
 
     elif viewer is not None:
         viewer.plot()
@@ -176,3 +186,7 @@ for step in range(steps):
 if viewer is not None:
     viewer.plot()
 input("Exit")
+
+plt.ioff()
+if fig is not None:
+    plt.close(fig)
